23942b238861bd17698a3269c73b7f7e231be41a
Rizvan <rizvan@MacBook-Air-2.fritz.box>
2023-08-30 08:16:36 +0200

    created credit calculator with ngModel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,34 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+    <h2>Credit Calculator</h2>
+    <div>
+      <label>Credit amount: </label>
+      <input type="text" [(ngModel)]="credit" />
+    </div>
+    <div>
+      <label>Rate: </label>
+      <select [(ngModel)]="rate">
+        <option>1</option>
+        <option>5</option>
+        <option>10</option>
+      </select>
+    </div>
+    <div>
+      <button (click)="myPayment()">Calculate</button>
+    </div>
+    <hr />
+    <h3>Monthly Payment:{{payment}}</h3>
+  `,
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'credit-calculator';
+  credit: number = 10000;
+  rate: number = 1;
+  payment: number = 0;
+
+  myPayment() {
+    this.payment = this.credit / this.rate;
+  }
 }
